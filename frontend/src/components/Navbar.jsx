81bc3760bfd0a81a1b4b4a9a7d522bbe2fa9437f
Bharti Patel <patelshree40@gmail.com>
2024-01-12 06:42:17 +0530

    backend initiated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
+import {useNavigate} from 'react-router-dom';
+
 function Navbar() {
+    
+    const navigate = useNavigate();
+
   return (
     <nav className=" text-slate-500 py-4 px-8 shadow-[0_.2px_10px_1px]">
         <div className='flex justify-between align-center '>
