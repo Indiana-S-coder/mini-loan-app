c9a3c49de72e108b1928bb356d9b0e64f5dd1a17
Bharti Patel <patelshree40@gmail.com>
2024-01-13 10:55:53 +0530

    loans operations created

---
+++ frontend/src/redux/loanReducer.js
@@ -0,0 +1,69 @@
+import {
+    GET_LOANS,
+    GET_LOAN,
+    ADD_LOAN,
+    PAY_LOAN,
+    GET_PAYMENT_HISTORY,
+    ITEMS_LOADING,
+    UPDATE_PAYMENT_STATUS
+} from './loanTypes';
+
+const initialState = {
+    loan: {},
+    loans: [],
+    payments: [],
+    loading: false,
+}
+
+export const loanReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_LOANS:
+            return {
+                ...state,
+                loans: action.payload,
+            }
+        
+        case GET_LOAN:
+            return {
+                ...state,
+                loan: action.payload,
+            }
+        
+        case ADD_LOAN:
+            return {
+                ...state,
+                loans: [action.payload, ...state.loans]
+            }
+
+        case PAY_LOAN:
+            return {
+                ...state,
+                loan: action.payload.loan,
+                payments: [action.payload.payment, ...state.payments],
+            }
+        
+        case GET_PAYMENT_HISTORY:
+            return {
+                ...state,
+                payments: action.payload,
+            }
+
+        case UPDATE_PAYMENT_STATUS:
+            return {
+                ...state,
+                loan: {
+                    ...state.loan,
+                    status: action.payload,
+                }
+            }
+        
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        
+        default:
+            return state;
+    }
+}
